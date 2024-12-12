7d66aa0d98c83f599f0131c01ceed78cfef18638
YoussefMohamed <ym0998682@gmail.comm>
2024-12-12 16:35:05 +0200

    navbar

---
+++ src/app/core/pages/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [ButtonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
